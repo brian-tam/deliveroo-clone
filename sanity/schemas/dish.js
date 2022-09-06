a6333f8b5daf9e4a7e4f8d7c4a3e95948dd4921a
Brian Tam <tamwinghei@Brians-MacBook-Pro.local>
2022-09-05 17:56:23 -0700

    add sanity client

---
+++ sanity/schemas/dish.js
@@ -0,0 +1,30 @@
+export default {
+  name: "dish",
+  title: "Dish",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Name of dish",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "short_desciprtion",
+      type: "string",
+      title: "Short Description",
+      validation: (Rule) => Rule.max(200),
+    },
+    {
+      name: "price",
+      type: "number",
+      title: "Price of the dish in USD",
+    },
+
+    {
+      name: "image",
+      type: "image",
+      title: "Image of the dish",
+    },
+  ],
+};
